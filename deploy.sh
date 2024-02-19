0d676c1152c04c8d503478f76f2d5091515f7e48
dongbin Shin <dongbin4013@naver.com>
2024-02-19 10:38:15 +0900

    디렉터리 리팩토리

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-skaffold run
-skaffold run -f monitoring-skaffold.yaml
-skaffold run -f auth-config-skaffold.yaml
+skaffold run -f istio/istio-system/istiod-skaffold.yaml
+skaffold run -f istio/istio-system/monitoring-skaffold.yaml
+skaffold run -f istio/istio-auth-module/auth-config-skaffold.yaml
+
+# add label for auth config resources
+kubectl label pods -l app=istio-ingressgateway jwt=enabled -n istio-system
 
 # change logging level temporarily
 # kubectl exec -it -n <namespace> <pod-name> exec -- curl -X POST http://localhost:15000/logging?level=trace
--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-kubectl delete -f kiali-dashboard.yaml
+kubectl delete -f istio/istio-system/manifests/kiali-dashboard.yaml
 # Wait for kiali deletion. Kiali must be deleted before kiali-operator.
 kubectl wait --for=delete svc/kiali -n istio-system
 kubectl wait --for=delete deployment/kiali -n istio-system
 
-skaffold delete -f auth-config-skaffold.yaml
-skaffold delete -f monitoring-skaffold.yaml
-skaffold delete
+skaffold delete -f istio/istio-auth-module/auth-config-skaffold.yaml
+skaffold delete -f istio/istio-system/monitoring-skaffold.yaml
+skaffold delete -f istio/istio-system/istiod-skaffold.yaml
